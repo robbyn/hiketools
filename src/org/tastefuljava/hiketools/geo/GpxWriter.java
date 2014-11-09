0bf851f8af8c6a110db8b500856d4bf0f1d9c364
Maurice Perry <maurice@perry.ch>
2014-11-09 20:10:21 +0100

    Tweaked default theme

--- src/org/tastefuljava/hiketools/geo/GpxWriter.java
+++ src/org/tastefuljava/hiketools/geo/GpxWriter.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.tastefuljava.hiketools.geo;
 
 import java.io.File;
@@ -13,10 +8,6 @@ import java.text.DateFormat;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 
-/**
- *
- * @author maurice
- */
 public class GpxWriter {
     private static final DateFormat TIME_FORMAT
             = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
